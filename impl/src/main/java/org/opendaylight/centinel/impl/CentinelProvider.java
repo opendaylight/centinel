fdb7f2254b9d892d9bbfbeab3a3602cfe445d453
Sumit Kapoor <sumit.kapoor@tcs.com>
2015-10-12 18:16:32 +0530

    Added partial implementation for setAlert functionality.
    Change-Id: I9cdce5d0030b6cca4a16cb36f6497057aa4a6e7e
    Signed-off-by: Sumit Kapoor <sumit.kapoor@tcs.com>

--- impl/src/main/java/org/opendaylight/centinel/impl/CentinelProvider.java
+++ impl/src/main/java/org/opendaylight/centinel/impl/CentinelProvider.java
@@ -8,22 +8,31 @@
 package org.opendaylight.centinel.impl;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.alertrule.rev150105.AlertruleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CentinelProvider implements BindingAwareProvider, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(CentinelProvider.class);
+    private RpcRegistration<AlertruleService> alertruleService;
 
     @Override
     public void onSessionInitiated(ProviderContext session) {
         LOG.info("CentinelProvider Session Initiated");
+        // alertruleService =
+        // session.addRpcImplementation(AlertruleService.class, new
+        // CentinelImpl());
     }
 
     @Override
     public void close() throws Exception {
         LOG.info("CentinelProvider Closed");
+        if (alertruleService != null) {
+            alertruleService.close();
+        }
     }
-
 }
+
