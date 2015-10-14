5a8653bf995a8ff37232d6f6fa01fdb91c5e7ad2
Sumit Kapoor <sumit.kapoor@tcs.com>
2015-10-14 16:27:46 +0530

    Added partial implementation of setAlert functionality.
    Change-Id: I9cdce5d0030b6cca4a16cb36f6497057aa4a6e7e
    Signed-off-by: Sumit Kapoor <sumit.kapoor@tcs.com>

--- impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/centinel/impl/rev141210/CentinelModule.java
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/centinel/impl/rev141210/CentinelModule.java
@@ -7,14 +7,32 @@
  */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210;
 
+import org.opendaylight.centinel.impl.CentinelImpl;
 import org.opendaylight.centinel.impl.CentinelProvider;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.alertrule.rev150105.AlertruleService;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CentinelModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.AbstractCentinelModule {
-    public CentinelModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+public class CentinelModule extends
+        org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.AbstractCentinelModule {
+    private static final Logger log = LoggerFactory.getLogger(CentinelModule.class);
+
+    public CentinelModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public CentinelModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.CentinelModule oldModule, java.lang.AutoCloseable oldInstance) {
+    public CentinelModule(
+            org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
+            org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.CentinelModule oldModule,
+            java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -25,9 +43,55 @@ public class CentinelModule extends org.opendaylight.yang.gen.v1.urn.opendayligh
 
     @Override
     public java.lang.AutoCloseable createInstance() {
+        final CentinelImpl centinelImpl = new CentinelImpl();
         CentinelProvider provider = new CentinelProvider();
+        DataBroker dataBrokerService = getDataBrokerDependency();
+        centinelImpl.setDataProvider(dataBrokerService);
+
+        final ListenerRegistration<DataChangeListener> alertMessageCountRuleChangeListener = dataBrokerService
+                .registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
+                        CentinelImpl.alertMessageCountRuleRecordId, centinelImpl, DataChangeScope.SUBTREE);
+
+        final ListenerRegistration<DataChangeListener> alertFeildContentRuleChangeListener = dataBrokerService
+                .registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
+                        CentinelImpl.alertFeildContentRuleRecordId, centinelImpl, DataChangeScope.SUBTREE);
+
+        final ListenerRegistration<DataChangeListener> alertFeildValueRuleChangeListener = dataBrokerService
+                .registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
+                        CentinelImpl.alertFieldValueRuleRecordId, centinelImpl, DataChangeScope.SUBTREE);
+
+        // final BindingAwareBroker.RpcRegistration<AlertruleService>
+        // rpcRegistration = getRpcRegistryDependency()
+        // .addRpcImplementation(AlertruleService.class, centinelImpl);
         getBrokerDependency().registerProvider(provider);
-        return provider;
+
+        final class AutoCloseableToaster implements AutoCloseable {
+
+            @Override
+            public void close() throws Exception {
+                alertMessageCountRuleChangeListener.close();
+                alertFeildValueRuleChangeListener.close();
+                alertFeildContentRuleChangeListener.close();
+                // rpcRegistration.close();
+                // runtimeReg.close();
+                closeQuietly(centinelImpl);
+                log.info("Toaster provider (instance {}) torn down.", this);
+            }
+
+            private void closeQuietly(final AutoCloseable resource) {
+                try {
+                    resource.close();
+                } catch (final Exception e) {
+                    log.debug("Ignoring exception while closing {}", resource, e);
+                }
+            }
+        }
+
+        AutoCloseable ret = new AutoCloseableToaster();
+        log.info("Toaster provider (instance {}) initialized.", ret);
+        return ret;
+
+        // return provider;
     }
 
 }
