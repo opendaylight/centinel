85f8991a3e9070fd8ae3808168ec3e923756db63
abhishek.abhi <abhishek.abhi@tcs.com>
2015-11-24 05:18:01 -0500

    Changes related to Module support for IMPL and LAAS modules.
    Change-Id: Ifeebf12f30b13b52cca59712c0a475e6ac8e2faf
    Signed-off-by: abhishek.abhi <abhishek.abhi@tcs.com>

--- impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/centinel/impl/rev141210/CentinelModule.java
+++ impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/centinel/impl/rev141210/CentinelModule.java
@@ -7,25 +7,30 @@
  */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210;
 
-import org.opendaylight.centinel.impl.CentinelImpl;
+import org.opendaylight.centinel.impl.CentinelAlertConditionImpl;
 import org.opendaylight.centinel.impl.CentinelProvider;
+import org.opendaylight.centinel.impl.CentinelStreamImpl;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.alertrule.rev150105.AlertruleService;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.stream.rev150105.StreamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CentinelModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.AbstractCentinelModule {
-	private static final Logger log = LoggerFactory.getLogger(CentinelModule.class);
-    public CentinelModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+public class CentinelModule extends
+        org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.AbstractCentinelModule {
+    private static final Logger log = LoggerFactory.getLogger(CentinelModule.class);
+
+    public CentinelModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public CentinelModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.CentinelModule oldModule, java.lang.AutoCloseable oldInstance) {
+    public CentinelModule(
+            org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver,
+            org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.centinel.impl.rev141210.CentinelModule oldModule,
+            java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -36,39 +41,33 @@ public class CentinelModule extends org.opendaylight.yang.gen.v1.urn.opendayligh
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-    	 final CentinelImpl centinelImpl = new CentinelImpl();
+        final CentinelAlertConditionImpl alertImpl = new CentinelAlertConditionImpl();
+        final CentinelStreamImpl streamImpl = new CentinelStreamImpl();
         CentinelProvider provider = new CentinelProvider();
         DataBroker dataBrokerService = getDataBrokerDependency();
-        centinelImpl.setDataProvider(dataBrokerService);
-         
-      final ListenerRegistration<DataChangeListener> alertMessageCountRuleChangeListener = dataBrokerService
-            .registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, CentinelImpl.alertMessageCountRuleRecordId,
-                    centinelImpl, DataChangeScope.SUBTREE);
-      
-     final ListenerRegistration<DataChangeListener> alertFeildContentRuleChangeListener = dataBrokerService
-         .registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, CentinelImpl.alertFeildContentRuleRecordId,
-                 centinelImpl, DataChangeScope.SUBTREE);
-     
-     
-     final ListenerRegistration<DataChangeListener> alertFeildValueRuleChangeListener = dataBrokerService
-             .registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, CentinelImpl.alertFieldValueRuleRecordId,
-                     centinelImpl, DataChangeScope.SUBTREE);
-     
-     final BindingAwareBroker.RpcRegistration<AlertruleService> rpcRegistration = getRpcRegistryDependency()
-              .addRpcImplementation(AlertruleService.class, centinelImpl);
-      getBrokerDependency().registerProvider(provider);
-        
-        final class AutoCloseableToaster implements AutoCloseable {
+
+        System.out.println("dataBrokerService created:" + dataBrokerService);
+        streamImpl.setDataProvider(dataBrokerService);
+
+        alertImpl.setDataProvider(dataBrokerService);
+       
+        final BindingAwareBroker.RpcRegistration<AlertruleService> rpcRegistration = getRpcRegistryDependency()
+                .addRpcImplementation(AlertruleService.class, alertImpl);
+
+        final BindingAwareBroker.RpcRegistration<StreamService> rpcRegistrationStream = getRpcRegistryDependency()
+                .addRpcImplementation(StreamService.class, streamImpl);
+
+        getBrokerDependency().registerProvider(provider);
+
+        final class AutoCloseableCentinel implements AutoCloseable {
 
             @Override
             public void close() throws Exception {
-            	alertMessageCountRuleChangeListener.close();
-            	alertFeildValueRuleChangeListener.close();
-            	alertFeildContentRuleChangeListener.close();
                 rpcRegistration.close();
-                //runtimeReg.close();
-                closeQuietly(centinelImpl);
-                log.info("Toaster provider (instance {}) torn down.", this);
+                rpcRegistrationStream.close();
+                closeQuietly(alertImpl);
+               
+                log.info(" provider (instance {}) torn down.", this);
             }
 
             private void closeQuietly(final AutoCloseable resource) {
@@ -80,11 +79,11 @@ public class CentinelModule extends org.opendaylight.yang.gen.v1.urn.opendayligh
             }
         }
 
-        AutoCloseable ret = new AutoCloseableToaster();
-        log.info("Toaster provider (instance {}) initialized.", ret);
+        AutoCloseable ret = new AutoCloseableCentinel();
+        log.info("Centinel provider (instance {}) initialized.", ret);
         return ret;
-        
-       // return provider;
+
+        // return provider;
     }
 
 }
---
+++ laas/src/main/java/org/opendaylight/laas/impl/CentinelEventNotifierImpl.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2015 Tata Consultancy services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.laas.impl;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.eventinput.rev150105.EventinputService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.streamhandler.rev150105.EventNotifiedBuilder;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class provides rpc implementation to publish graylog events to md-sal.
+ * 
+ * @author Monika Verma
+ * 
+ */
+public class CentinelEventNotifierImpl implements AutoCloseable, EventinputService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CentinelEventNotifierImpl.class);
+
+    private NotificationProviderService notificationProvider;
+    private final ExecutorService executor;
+
+    public CentinelEventNotifierImpl() {
+        executor = Executors.newFixedThreadPool(1);
+    }
+
+    public void setNotificationProvider(final NotificationProviderService salService) {
+        this.notificationProvider = salService;
+        LOG.info("notifictaion provider set");
+    }
+
+    @Override
+    public void close() throws Exception {
+        executor.shutdown();
+
+    }
+
+    @Override
+    public Future<RpcResult<Void>> notifyEvent(
+            org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.eventinput.rev150105.NotifyEventInput input) {
+
+        LOG.info("Input for rpc notifyEvent : " + input);
+
+        notificationProvider.publish(new EventNotifiedBuilder().setEventType(input.getEventType())
+                .setEventBodyType(input.getEventBodyType()).setEventBody(input.getEventBody())
+                .setEventKeys(input.getEventKeys()).build());
+
+        return null;
+    }
+
+}
