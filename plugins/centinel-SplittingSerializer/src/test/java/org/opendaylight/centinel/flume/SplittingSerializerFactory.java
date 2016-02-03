952463f88ed0ea33c92ef7200f5a265330a50a60
AmandeepSingh <amandeep.singh6@tcs.com>
2016-02-03 17:51:46 +0530

    Added test cases for SplittingSerializer plugin
    Change-Id: Iaa418c89815505d1dce837d17148bfbdfd92adde
    Signed-off-by: AmandeepSingh <amandeep.singh6@tcs.com>

---
+++ plugins/centinel-SplittingSerializer/src/test/java/org/opendaylight/centinel/flume/SplittingSerializerFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Tata Consultancy services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.centinel.flume;
+
+import static org.mockito.Mockito.mock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.flume.Event;
+
+/**
+ * @author Amandeep Singh 
+ * 
+ * 			 This class provides the dummy inputs for test case
+ *           
+ */
+
+public class SplittingSerializerFactory {
+	
+	byte[] table = "centinel".getBytes();
+	byte[] cf = "columnFamily".getBytes();	
+	Map<String, String> map =new HashMap<String, String>();
+	Event mockEvent=mock(Event.class);
+	
+	public Event flumeEvent()
+	{
+		map.put("eventType", "stream");
+		return mockEvent;
+		
+	}
+	
+
+}
