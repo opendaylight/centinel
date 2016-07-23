4b33cfa2895ae970094a8b57e8bf25d049b781da
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-07-22 23:39:10 -0400

    Add missing license headers
    Change-Id: I7e49655cd0114695ac4395d6a7fe9621cd92898a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- streamhandler/src/test/java/org/opendaylight/centinel/impl/dashboard/StreamCounterInfoCacheFactory.java
+++ streamhandler/src/test/java/org/opendaylight/centinel/impl/dashboard/StreamCounterInfoCacheFactory.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016 Tata Consultancy Services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.centinel.impl.dashboard;
 
 import java.util.Calendar;
@@ -9,8 +16,8 @@ import org.opendaylight.streamhandler.impl.StreamhandlerImpl;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.subscribe.rev150105.Subscribe.Mode;
 
 public class StreamCounterInfoCacheFactory {
-	
-	
+
+
 	public String removeCounterForWidgetInput()
 	{
 		return "1001";
